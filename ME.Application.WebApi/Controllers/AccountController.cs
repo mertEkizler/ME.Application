7909a9e93fd6c7cac7f9307c233e1d2c5d8e384d
mertekizler1 <mert.ekizler@valprovia.com>
2021-10-24 15:21:18 +0300

    added new View

---
+++ ME.Application.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using ME.Application.WebApi.Data.Context;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ME.Application.WebApi.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationContext _context;
+
+        public AccountController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Create(int id)
+        {
+            var userInfo = _context.ApplicationUsers.SingleOrDefault(x => x.Id == id);
+
+            return View(userInfo);
+        }
+    }
+}
---
+++ ME.Application.WebApi/TagHelpers/GetAccountCount.cs
@@ -0,0 +1,25 @@
+﻿using ME.Application.WebApi.Data.Context;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Linq;
+
+namespace ME.Application.WebApi.TagHelpers
+{
+    [HtmlTargetElement("getAccountCount")]
+    public class GetAccountCount : TagHelper
+    {
+        public int ApplicationUserId { get; set; }
+        private readonly ApplicationContext _context;
+
+        public GetAccountCount(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var accountCount = _context.Accounts.Count(x => x.ApplicationUserId == ApplicationUserId);
+            var html = $"<span class='badge bg-danger'>{accountCount} </span>";
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
